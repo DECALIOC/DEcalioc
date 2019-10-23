b96b78e93a71b9a2cee2b40efdfc381d19b8234f
PurpleCrumpets <timchurchfield96@gmail.com>
2019-10-23 10:56:44 +0200

    Create job.sh

---
+++ DEcalioc/DEMmodels/Lift100/job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -q default
+#PBS -j oe
+#PBS -l nodes=1:ppn=0,walltime=00:00:00
+#PBS -N DEM
+#PBS -o ${PBS_JOBNAME}_output.txt
+
+cd path
+
+mpirun -np 0 lmp_auto < in.rotatingdrum
