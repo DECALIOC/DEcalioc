b095cb2eac67f0048b9326ba8b81845b6e59d97e
PurpleCrumpets <timchurchfield96@gmail.com>
2019-10-23 15:56:25 +0200

    Update startSimulation.m

--- DEcalioc/infrastructure/startSimulation.m
+++ DEcalioc/infrastructure/startSimulation.m
@@ -37,6 +37,11 @@ function startSimulation(model,folderName)
   chdir([path, 'optim/', model, '/', folderName, '/']);    
 %  status= unix(['sh ', path, 'optim/', model, '/', folderName, '/runscript']);
   status= unix(['sh ', path, 'optim/', model, '/', folderName, '/job.sh']);
+  
+  status= unix(['cd ', path, 'optim/', model, '/', folderName, ';',...
+    ' /opt/torque/bin/qsub job.sh']);
+    
+    /data/home/church70/GitHub/DEcalioc/DEcalioc/DEMmodels/Lift100$
   chdir(path);
   
 endfunction
