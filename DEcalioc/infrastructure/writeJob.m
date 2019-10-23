f01ce29969846bef147abc1ee21d7923df7d70c2
PurpleCrumpets <timchurchfield96@gmail.com>
2019-10-23 10:57:00 +0200

    Create writeJob.m

---
+++ DEcalioc/infrastructure/writeJob.m
@@ -0,0 +1,11 @@
+function [] = writeJob() 
+  
+  % editJob.m
+  %
+  % Function to read in the bash script job.sh and edit the required sections to
+  % successfully submit a LIGGGHTS simulation as a separate job to the cluster
+  %
+  %
+  % Tim Churchfield
+  %
+  % Last edited: 23/10/2019
