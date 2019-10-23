c9ad8faf1f385f6f3078d40c2ae8adc1f363f53c
PurpleCrumpets <timchurchfield96@gmail.com>
2019-10-23 12:38:30 +0200

    Create simDataFromHead.m

---
+++ DEcalioc/infrastructure/simDataFromHead.m
@@ -0,0 +1,59 @@
+function simData = simDataFromHead(model, folderName)
+  
+  % simDataFromHead.m
+  %
+  % Function to obtain simulation settings from data.head.
+  %
+  %
+  % Tim Churchfield
+  %
+  % Last edited: 23/10/2019
+  
+  
+  
+  global path;
+  
+  % open data.head
+%  fd = fopen([path, 'optim/', model, '/', folderName, '/data.head'], 'r');
+  
+  fd = fopen('M:\fwdf\members\church70\MATLAB_Integration\start\template\data.head','r');
+  
+  str = fgets(fd);   
+  % Walltime
+  while ~startsWith(str,'variable walltime')
+      str = fgets(fd);
+  end
+  walltime = sscanf(str,'variable walltime string "%s');
+  walltime = walltime(1:end-1);
+
+  % Processors
+  frewind(fd); % Reset read position in data.head
+  while ~startsWith(str,'variable proc ')
+      str = fgets(fd);
+  end
+  processors = sscanf(str,'variable proc equal %d');
+
+  % Queue
+  frewind(fd); % Reset read position in data.head
+  while ~startsWith(str,'variable queue ')
+      str = fgets(fd);
+  end
+  queue = sscanf(str,'variable queue string "%s');
+  queue = queue(1:end-1);
+
+  % Nodes
+  frewind(fd); % Reset read position in data.head
+  while ~startsWith(str,'variable nodes ')
+      str = fgets(fd);
+  end
+  nodes = sscanf(str,'variable nodes equal %d');
+
+  mpiProc = processors*nodes;
+  
+  % Record simulation settings
+  simData = {walltime; processors; queue; nodes; mpiProc};
+
+  % Close data.head
+  fclose(fd);
+
+endfunction
